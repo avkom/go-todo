90a51e25d988c5033fcefe807947e8b542e346c7
Oleksandr Komendant <avkomq@gmail.com>
2019-07-15 19:56:30 -0400

    Write an error to the HTTP response body

--- todo-service/taskController.go
+++ todo-service/taskController.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -38,26 +39,37 @@ func (c *TaskControllerImpl) getByID(w http.ResponseWriter, r *http.Request) {
 func (c *TaskControllerImpl) create(w http.ResponseWriter, r *http.Request) {
 	var task TaskData
 	json.NewDecoder(r.Body).Decode(&task)
-	c.s.Create(task)
+	err := c.s.Create(task)
+	writeEmptyResponse(w, err)
 }
 
 func (c *TaskControllerImpl) update(w http.ResponseWriter, r *http.Request) {
 	var task TaskData
 	json.NewDecoder(r.Body).Decode(&task)
-	c.s.Update(task)
+	err := c.s.Update(task)
+	writeEmptyResponse(w, err)
 }
 
 func (c *TaskControllerImpl) delete(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
-	c.s.Delete(id)
+	err := c.s.Delete(id)
+	writeEmptyResponse(w, err)
 }
 
 func writeJSONResponse(w http.ResponseWriter, response interface{}, err error) {
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	json.NewEncoder(w).Encode(response)
+}
+
+func writeEmptyResponse(w http.ResponseWriter, err error) {
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+	}
 }
