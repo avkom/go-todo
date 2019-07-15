35cdf308a0126a7171599c53e8cc4079f41329e4
Oleksandr Komendant <avkomq@gmail.com>
2019-07-15 19:19:18 -0400

    Create table `tasks` on application startup

--- todo-service/main.go
+++ todo-service/main.go
@@ -19,10 +19,25 @@ func main() {
 		log.Fatal(err)
 	}
 
+	err = initDB(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	taskRepository := NewSQLTaskRepository(db)
 	taskService := NewTaskService(taskRepository)
 	taskController := NewTaskController(taskService)
 
 	http.Handle("/", taskController)
 	http.ListenAndServe(":8080", nil)
+}
+
+func initDB(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS tasks (
+		id VARCHAR(50) NOT NULL PRIMARY KEY,
+		title VARCHAR(100) NOT NULL,
+		description TEXT
+	)`
+	_, err := db.Exec(query)
+	return err
 }
